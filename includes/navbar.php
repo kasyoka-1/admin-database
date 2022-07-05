321d802b08ed1e34f2ea12aef25aec926bd1f498
kasyoka-1 <mkasyoka1@gmail.com>
2022-07-05 13:22:03 +0300

    added separate file parts

---
+++ includes/navbar.php
@@ -0,0 +1,3 @@
+<div class="header">
+	<img src="images/download.png" alt="image preview" height="50" width="50" class="rounded-circle">
+</div>
---
+++ logics/dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "web2";
+
+
+// the variable $conn can be written as anything and still work but the have to be the same yaani $conn $conn or $mk $mk
+$conn = mysqli_connect($server,$username,$password,$database);
+
+?>
