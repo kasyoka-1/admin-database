1c56f9e2f82dcc396cfafb0735f0d613d26d4318
kasyoka-1 <mkasyoka1@gmail.com>
2022-07-08 13:23:28 +0300

    added form update

--- edit-enrollment.php
+++ edit-enrollment.php
@@ -4,8 +4,40 @@ $queryuser = mysqli_query($conn,"SELECT * FROM enrollment WHERE no='".$_GET['id'
 while($fetchuser=mysqli_fetch_array($queryuser))
 {
     $fullname = $fetchuser['fullname'];
+    $phonenumber = $fetchuser['phonenumber'];
+    $email = $fetchuser['email'];
+    $gender = $fetchuser['gender'];
+    $course = $fetchuser['course'];
     
 }
+
+// update user records
+if(isset($_POST["updatebutton"]))
+{
+    // fetch form data
+    $name = $_POST["fullname"];
+    $phone = $_POST["phonenumber"];
+    $emailAddress = $_POST["email"];
+    $formgender = $_POST["gender"];
+    $formcourse = $_POST["course"];
+
+
+    // update records
+    $updatequery = mysqli_query($conn, "UPDATE enrollment SET fullname='$name', phonenumber='$phone', email='$emailAddress', 
+    gender='$formgender', course='$formcourse' WHERE no='".$_GET['id']."'");
+
+    if($updatequery)
+    {
+        echo "Data Updated";
+    }
+    else
+    {
+        echo "Error occured";
+    }
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -27,6 +59,51 @@ while($fetchuser=mysqli_fetch_array($queryuser))
                     <div class="card">
                         <div class="card-header bg-dark text-white text-center">
                             <h4>Edit Student: <?php echo $fullname ?></h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="edit-enrollment.php" method="POST" class="container">
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="fullname" class="form-label">Full Name:</label>
+                                            <input type="text" name="fullname" value="<?php echo $fullname?>" class="form-control" placeholder="Enter your full name">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="phonenumber" class="form-label">Phone Number:</label>
+                                            <input type="text" name="phonenumber" value="<?php echo $phonenumber?>" class="form-control" placeholder="+2547...">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="email" class="form-label">Email Address:</label>
+                                            <input type="text" name="email" value="<?php echo $email?>" class="form-control" placeholder="Please enter your email address">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="gender" class="form-label">Gender:</label><br>
+                                            <select name="gender" class="form-control" name="gender" aria-label="Default select example">
+                                                <option value="<?php echo $gender?>">Male</option>
+                                                <option value="Female">Female</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="course" class="form-label">Course:</label><br>
+                                            <select class="form-control" name="course"  aria-label="Default select example">
+                                                <option value="<?php echo $course?>">Software Development</option>
+                                                <option value="Data analysis">Data analysis</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="mb-3 col-lg-12">
+                                            <button style="margin-top:10px" type="submit" name="updatebutton" class="btn btn-primary">Update</button>
+                                        </div>
+                                    </div>
+                            </form>
                         </div>
                     </div>
                 </div>
