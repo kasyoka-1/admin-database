e56c1a134a04f16265b32cadbbb29e1062407c36
kasyoka-1 <mkasyoka1@gmail.com>
2022-07-15 13:52:31 +0300

    added contactus

--- contactus.php
+++ contactus.php
@@ -1,65 +1,57 @@
 <?php
- require_once("logics/dbconnection.php");
+require_once("logics/dbconnection.php");
 
-$sqliQuery = mysqli_query($conn,"SELECT * FROM enrollment");
-
-
-
+$sqliQuery = mysqli_query($conn,"SELECT * FROM contactus");
 ?>
+
+
+
+
+
+
+
 
 
 <!DOCTYPE html>
 <html>
 <?php require_once("includes/headers.php")?>
 <body>
-	<!-- All our code. write here   -->
 	<?php require_once("includes/navbar.php")?>
 
-
 	<div class="sidebar">
-	<?php require_once("includes/sidebar.php")?>
+	    <?php require_once("includes/sidebar.php")?>
+    </div>
 
-	</div>
-
-	<div class="main-content">
+    <div class="main-content">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="card-header bg-dark text-white text-center">
-						<span>Students</span>
+						<span>Messages</span>
 					</div>
                     <div class="card-body">
                         <table class="table table-striped table-hover table-responsive" style="font-style: 12px">
                             <thead>
                                 <tr>
                                     <th>No.</th>
-                                    <th>Full Name</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
                                     <th>Phone Number</th>
                                     <th>Email</th>
-                                    <th>Gender</th>
-                                    <th>Course</th>
-                                    <th>Enrolled On</th>
-                                    <th>Actions</th>
+                                    <th>Your Message</th>
                                 </tr>
                             </thead>
                             <tbody>
 								<?php  while($fetch = mysqli_fetch_array($sqliQuery)) {?>
 									<tr>
 										<td><?php echo $fetch['no']  ?></td>
-										<td><?php echo $fetch['fullname']  ?></td>
+										<td><?php echo $fetch['firstname']  ?></td>
+										<td><?php echo $fetch['lastname']  ?></td>
 										<td><?php echo $fetch['phonenumber']  ?></td>
 										<td><?php echo $fetch['email']  ?></td>
-										<td><?php echo $fetch['gender']  ?></td>
-										<td><?php echo $fetch['course']  ?></td>
-										<td><?php echo $fetch['created_at']  ?></td>
+										<td><?php echo $fetch['message']  ?></td>
 										<td>
-											<a href="edit-enrollment.php?id=<?php echo $fetch['no']  ?>" class="btn btn-primary btn-sm">
-												<i class="fa fa-edit"></i>
-											</a>
-											<a href="view-enrollment.php?id=<?php echo $fetch['no'] ?>" class="btn btn-info btn-sm">
-												<i class="fa fa-eye"></i>
-											</a>
-											<a href="delete-enrollment.php?id=<?php echo $fetch['no']?>" class="btn btn-danger btn-sm">
+											<a href="#" class="btn btn-danger btn-sm">
 												<i class="fa fa-trash"></i>
 											</a>
                                     	</td>
@@ -76,9 +68,7 @@ $sqliQuery = mysqli_query($conn,"SELECT * FROM enrollment");
 
 
 
-	<?php require_once("includes/scripts.php")?>
+    <?php require_once("includes/scripts.php")?>
 
 </body>
 </html>
-
-
